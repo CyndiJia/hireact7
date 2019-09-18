71e8b87169085204d99ac8ea5bdff3726dbd3d58
Cyndi <31037969+CyndiJia@users.noreply.github.com>
2019-09-18 11:08:28 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//import { directive } from '@babel/types';
+
+function App() {
+  return(
+  <div>
+    <h1>Someone in the crowd could be the one you need to know.</h1>
+    <img src="https://upload-images.jianshu.io/upload_images/3002012-d1071390b4fc34ac.jpg" alt="lalaland"></img>
+    <a href = "https://www.youtube.com/watch?v=0pdqf4P9MB8"><img id = "right" src = "https://cdn1.imggmi.com/uploads/2019/9/13/7ca28a128802f0b9a8590e62c4939f91-full.png" width = "80"></img></a>
+  </div>
+  );
+}
+
+export default App;
